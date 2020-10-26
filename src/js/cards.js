6dc03066cea4501831ff99721e537e190325b1e0
Alena <alena.senkiv@gmail.com>
2020-10-26 19:47:05 +0200

    add toggle

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,7 +1,7 @@
-import cardsTpl from '../templates/cards.hbs'
+import cardsTpl from '../templates/cards.hbs';
 import items from '../menu.json';
 
-const menuContainer = document.querySelector('.js-menu')
+const menuContainer = document.querySelector('.js-menu');
 menuContainer.innerHTML = createCardsMarkup(items);
 
 function createCardsMarkup(items) {
--- src/js/theme.js
+++ src/js/theme.js
@@ -4,13 +4,24 @@ const Theme = {
 };
 
 const toggleSwitch = document.querySelector('#theme-switch-toggle');
-const bodyTheme = document.body;
+const bodyStyle = document.body.classList;
 
+onActiveTheme();
 toggleSwitch.addEventListener('change', onToggleTheme);
 
-function onToggleTheme() {
-  const activeTheme = bodyTheme.classList.toggle(Theme.DARK);
-  localStorage.setItem('theme', activeTheme);
+function onToggleTheme(e) {
+  e.currentTarget.checked
+    ? bodyStyle.add(Theme.DARK) & bodyStyle.remove(Theme.LIGHT)
+    : bodyStyle.add(Theme.LIGHT) & bodyStyle.remove(Theme.DARK);
+  localStorage.setItem('theme', bodyStyle.value);
 }
 
-
+function onActiveTheme() {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+    bodyStyle.add(savedTheme);
+  }
+  if (bodyStyle.value === Theme.DARK) {
+    toggleSwitch.checked = true;
+  }
+}
